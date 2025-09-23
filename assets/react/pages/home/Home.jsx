0b05b9c90d04b202ba714db77db6776acfd7b3ea
melodieyeye <yeyemelodie@outlook.fr>
2025-09-23 11:46:29 +0200

    update Home page and Tailwind setup with DaisyUI

--- assets/react/pages/home/Home.jsx
+++ assets/react/pages/home/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
         <div className="container mx-auto p-4">
             <h1 className="text-3xl font-bold mb-4">Welcome to the Home Page</h1>
             <p className="text-lg">This is the main landing page of the application.</p>
-            <button type="button" onClick={apiService.logout}>Deconnexion</button>
+            <button className="btn" onClick={apiService.logout}>Deconnexion</button>
         </div>
     );
 }
