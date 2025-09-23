a2cccc8bc1ba0ccab96603e211671c068a25ba22
melodieyeye <yeyemelodie@outlook.fr>
2025-09-23 09:47:35 +0200

    improve Textfield class handling and login form styles

--- assets/react/components/Textfield.jsx
+++ assets/react/components/Textfield.jsx
@@ -3,31 +3,27 @@ import React from "react";
 const Textfield = ({
                        type = "text",
                        placeholder,
-                       size = "md",
                        value,
                        onChange,
                        isError = false,
                        errorCaption = "",
                        required = false,
+                       inputClass = "",
                    }) => {
-    const sizeClasses = {
-        sm: "text-sm h-8",
-        md: "text-sm h-10",
-    };
 
     return (
-        <div className="form-control w-full mb-4">
+        <div className="form-control w-full">
             <input
                 type={type}
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
                 required={required}
-                className={`input input-${size} w-full ${isError ? "input-error" : "input-bordered"}`}
+                className={`${inputClass} ${isError ? "input-error" : "input-bordered"}`}
             />
             {isError && errorCaption && (
                 <label className="label">
-                    <span className="label-text-alt text-error">{errorCaption}</span>
+                    <span className="label-text-alt text-red-500">{errorCaption}</span>
                 </label>
             )}
         </div>
